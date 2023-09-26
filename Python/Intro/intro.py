8cb66148d4366727febc495d9aa544c3c668e689
ronald116 <ronaldbotchway@gmail.com>
2023-09-26 03:01:48 +0000

    intro

---
+++ Python/Intro/intro.py
@@ -0,0 +1,11 @@
+#Numbers
+print(2 + 2)
+print(50 - 5 * 6)
+print((50 - 5 * 6) / 4)
+
+#lists
+a = [1,2,3,4,5,6]
+print(a[0]) #to print the first index in the list
+print(a[-1]) #to print the last index in the list
+print(a[2:5]) # to print out from index 2 to 4(exclude index 5)
+ 
