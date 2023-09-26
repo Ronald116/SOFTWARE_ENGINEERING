04d21d2ff65f633393a1de8494546b65a59d6f97
ronald116 <ronaldbotchway@gmail.com>
2023-09-26 03:06:28 +0000

    intro update

--- Python/Intro/intro.py
+++ Python/Intro/intro.py
@@ -8,4 +8,11 @@ a = [1,2,3,4,5,6]
 print(a[0]) #to print the first index in the list
 print(a[-1]) #to print the last index in the list
 print(a[2:5]) # to print out from index 2 to 4(exclude index 5)
- 
+
+#this program says hello and asks for my name
+print("Hello World!")
+print("What is your name?")
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
